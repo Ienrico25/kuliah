a53220b9c89e9121fae559ea39a47bf7af62b853
Ienrico25 <113285885+Ienrico25@users.noreply.github.com>
2022-09-13 22:14:32 +0700

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,12 @@
+greeting=input("Say something to greet the customer:")
+g=greeting.capitalize().strip()
+if(g=="Hello"):
+    m=0
+elif(g[0]=="Hello"):
+    m=0
+elif(g[0]=="H"):
+    m=20
+else:
+    m=100
+
+print("You must pay:$" + str(m))
